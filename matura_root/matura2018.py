36de017326b58268fba7bc5da29877038ea7cefa
Kasia <efefre@icloud.com>
2019-10-03 14:01:59 +0200

    Add read_txt function

---
+++ matura_root/matura2018.py
@@ -0,0 +1,6 @@
+# Arkusz: https://cke.gov.pl/images/_EGZAMIN_MATURALNY_OD_2015/Arkusze_egzaminacyjne/2018/formula_od_2015/informatyka/MIN-R2_1P-182.pdf
+
+def read_txt(filename):
+    file = open(filename).readlines()
+    return file
+
