6be73125b657ea79c3bc7a879773349e57994f8c
Kasia <efefre@icloud.com>
2019-10-16 21:16:49 +0200

    Add file to read, file name to save results and check if file already exists

--- matura_root/matura2019.py
+++ matura_root/matura2019.py
@@ -1,4 +1,6 @@
 # Arkusz: https://cke.gov.pl/images/_EGZAMIN_MATURALNY_OD_2015/Arkusze_egzaminacyjne/2019/formula_od_2015/informatyka/MIN-R2_1P-192.pdf
+import os
+
 def read_txt(filename):
     file = open(filename).readlines()
     return file
@@ -6,3 +8,11 @@ def read_txt(filename):
 def save_txt(filename, text):
     with open(filename,'a') as text_file:
         text_file.write(text)
+
+if __name__ == '__main__':
+    file = read_txt('liczby.txt')
+    filename = '2019_wynik4.txt'
+
+    if os.path.isfile(filename):
+        os.remove(filename)
+
