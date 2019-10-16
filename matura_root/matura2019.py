b1bb442dcb4ef844b6fd1c3aadc06c80a1d805e6
Kasia <efefre@icloud.com>
2019-10-16 21:13:17 +0200

    Add read_txt function

---
+++ matura_root/matura2019.py
@@ -0,0 +1,4 @@
+# Arkusz: https://cke.gov.pl/images/_EGZAMIN_MATURALNY_OD_2015/Arkusze_egzaminacyjne/2019/formula_od_2015/informatyka/MIN-R2_1P-192.pdf
+def read_txt(filename):
+    file = open(filename).readlines()
+    return file
