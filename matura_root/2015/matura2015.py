ffd9b93fb85e867ca1a6c4ab352e1c02404da22f
Kasia <efefre@icloud.com>
2019-12-30 16:39:15 +0100

    Import data from file to list

---
+++ matura_root/2015/matura2015.py
@@ -0,0 +1,26 @@
+# Arkusz: https://cke.gov.pl/images/_EGZAMIN_MATURALNY_OD_2015/Arkusze_egzaminacyjne/2015/formula_od_2015/MIN-R2_1P-152.pdf
+
+import os
+import string
+
+def read_txt(file):
+    file = open(file)
+    return file
+
+
+def save_txt(file, text):
+    with open(file, 'a') as text_file:
+        text_file.write(text)
+
+if __name__ == '__main__':
+
+    numbers = read_txt('2015/liczby.txt')
+    filename = '2015/2015_wynik_4.txt'
+
+    if os.path.isfile(filename):
+        os.remove(filename)
+
+    numbers_list = []
+
+    for line in numbers:
+        numbers_list.append(line.rstrip('\n'))
