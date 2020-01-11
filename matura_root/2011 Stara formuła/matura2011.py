e1235a6f6d844bded55df41f347ec96bb321ec34
Kasia <efefre@icloud.com>
2020-01-11 14:19:32 +0100

    Import data from file

--- matura_root/2011 Stara formuła/matura2011.py
+++ matura_root/2011 Stara formuła/matura2011.py
@@ -10,3 +10,12 @@ def read_txt(filename):
 def save_txt(file, text):
     with open(file, 'a') as text_file:
         text_file.write(text)
+
+if __name__ == '__main__':
+    numbers = read_txt('2011 Stara formuła/liczby.txt')
+    filename= '2011 Stara formuła/2011_wynik_6.txt'
+
+    if os.path.isfile(filename):
+        os.remove(filename)
+
+    numbers = [line.rstrip('\n') for line in numbers]
