08972e074b65574339ae33ecad1fda71da5749e1
Kasia <efefre@icloud.com>
2020-01-12 10:36:10 +0100

    Import data from file

---
+++ matura_root/2009 Stara formuła/matura2009.py
@@ -0,0 +1,22 @@
+# Arkusz: https://cke.gov.pl/images/_EGZAMIN_MATURALNY_OD_2015/Arkusze_egzaminacyjne/2009/informatyka/PR_2.pdf
+
+import os
+
+def read_txt(filename):
+    with open(filename, 'r') as f:
+        file = f.readlines()
+        return file
+
+def save_txt(file, text):
+    with open(file, 'a') as text_file:
+        text_file.write(text)
+
+
+if __name__ == '__main__':
+    data = [line.rstrip('\n').split() for line in read_txt('2009 Stara formuła/dane.txt')]
+    filename_a = '2009 Stara formuła/2009_zad_5.txt'
+    filename_b = '2009 Stara formuła/2009_slowa.txt'
+
+    for filename in (filename_a, filename_b):
+        if os.path.isfile(filename):
+            os.remove(filename)
