f88223e91db16a6eb8ca0c29d984712bd2a2ee9e
Kasia <efefre@icloud.com>
2020-01-12 15:08:09 +0100

    Update filenames

--- matura_root/2008 Stara formuła/matura2008.py
+++ matura_root/2008 Stara formuła/matura2008.py
@@ -15,7 +15,8 @@ if __name__ == '__main__':
     words = [line.rstrip('\n') for line in read_txt('2008 Stara formuła/slowa.txt')]
     filename_a = '2008 Stara formuła/2008_hasla_a.txt'
     filename_b = '2008 Stara formuła/2008_hasla_b.txt'
-    filename_c = '2008 Stara formuła/2008_slowa_b.txt'
+    filename_c = '2008 Stara formuła/2008_slowa_a.txt'
+    filename_d = '2008 Stara formuła/2008_slowa_b.txt'
 
     for filename in (filename_a, filename_b, filename_c):
         if os.path.isfile(filename):
